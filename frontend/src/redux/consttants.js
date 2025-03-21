18f626c8b02b15f472d5ccfc9e2dc5c539eac762
swamiankit261 <swamiankit261@gmail.com>
2025-03-21 18:26:08 +0530

    update BASE_URL to use VITE_BACKEND_URL from environment variables

--- frontend/src/redux/consttants.js
+++ frontend/src/redux/consttants.js
@@ -1,4 +1,5 @@
-export const BASE_URL = '';
+// eslint-disable-next-line no-undef
+export const BASE_URL = process.env.VITE_BACKEND_URL;
 export const USERS_URL = "/api/v1/user";
 export const PRODUCT_URL = "/api/v1/product";
 export const ORDER_URL = "/api/v1/order";
